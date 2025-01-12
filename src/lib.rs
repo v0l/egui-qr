f9cf52b7eae353fa9e59ed0358151211d48824d1
kieran <kieran@harkin.me>
2025-01-12 11:00:18 +0000

    quiet zone

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,14 @@ enum CodeSrc<'a> {
 
 pub struct QrCodeWidget<'a> {
     code: CodeSrc<'a>,
+    quiet_zone: f32,
 }
 
 impl<'a> QrCodeWidget<'a> {
     pub fn new(code: &'a QrCode) -> Self {
         QrCodeWidget {
             code: CodeSrc::Ref(code),
+            quiet_zone: 1.,
         }
     }
 
@@ -22,31 +24,37 @@ impl<'a> QrCodeWidget<'a> {
         let code = QrCode::new(data)?;
         Ok(QrCodeWidget {
             code: CodeSrc::Owned(code),
+            quiet_zone: 1.,
         })
+    }
+
+    pub fn quiet_zone(mut self, quiet_zone: f32) -> Self {
+        self.quiet_zone = quiet_zone;
+        self
     }
 }
 
 impl Widget for QrCodeWidget<'_> {
     fn ui(self, ui: &mut Ui) -> Response {
-        let size = ui.available_size();
+        let outer_size = ui.available_size();
 
         let code_ref = match &self.code {
             CodeSrc::Ref(code) => code,
             CodeSrc::Owned(q) => q,
         };
         let w = code_ref.width();
-        let scale = (size.x.min(size.y) / w as f32).floor();
-        let start = ui.cursor().min;
-        let (response, painter) =
-            ui.allocate_painter(vec2(w as f32 * scale, w as f32 * scale), Sense::click());
+        let min_size = outer_size.x.min(outer_size.y);
+        let scale = min_size / (w as f32 + (self.quiet_zone * 2.));
+        let start = ui.cursor().min + vec2(self.quiet_zone * scale, self.quiet_zone * scale);
+        let (response, painter) = ui.allocate_painter(vec2(min_size, min_size), Sense::click());
 
         painter.rect(response.rect, Rounding::ZERO, Color32::WHITE, Stroke::NONE);
         let mut ctr = 0;
         for c in code_ref.to_colors() {
             let row = ctr / w;
             let col = ctr % w;
-            let c_start = start + vec2(col as f32 * scale, row as f32 * scale);
-            let c_end = c_start + vec2(scale, scale);
+            let c_start = start.floor() + vec2(col as f32 * scale, row as f32 * scale).floor();
+            let c_end = c_start.ceil() + vec2(scale, scale).ceil();
             if matches!(c, Color::Dark) {
                 painter.rect(
                     Rect::from_min_max(c_start, c_end),
